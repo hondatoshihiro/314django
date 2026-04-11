1cde7486d91675932c769e1880bb7ac503dbfa0c
hondatoshihiro <harutokaho@gmail.com>
2026-04-12 05:53:06 +0900

    アプリ内のurls.pyを作成、プロジェクトのurls.pyはアプリ内のurls.pyをインクルード

--- gt/gt/urls.py
+++ gt/gt/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-#recordlaptimeアプリのviews.pyをrecordlaptimeとしてインポートする。
-import recordlaptime.views as recordlaptime
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #recordlaptimeアプリのviews.pyのindexメソッドを呼び出す。
-    path('recordlaptime/', recordlaptime.index),
+    #recordlaptimeアプリのurls.pyをインクルードする。
+    path('recordlaptime/', include('recordlaptime.urls')),
 ]
---
+++ gt/recordlaptime/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+#この記載はなに(カレントディレクトリから、viewsを読み込むってこと)
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
