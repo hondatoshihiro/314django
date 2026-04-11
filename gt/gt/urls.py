1d16754c00548be1706fbff53d443f703c49b9ad
hondatoshihiro <harutokaho@gmail.com>
2026-04-12 05:31:00 +0900

    urls.pyの練習

--- gt/gt/urls.py
+++ gt/gt/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#recordlaptimeアプリのviews.pyをrecordlaptimeとしてインポートする。
+import recordlaptime.views as recordlaptime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #recordlaptimeアプリのviews.pyのindexメソッドを呼び出す。
+    path('recordlaptime/', recordlaptime.index),
 ]
---
+++ gt/recordlaptime/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RecordlaptimeConfig(AppConfig):
+    name = 'recordlaptime'
