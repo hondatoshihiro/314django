cc2a0df8e656ece012f2ed345d71c1bf66104fb3
hondatoshihiro <harutokaho@gmail.com>
2026-04-12 11:07:41 +0900

    urlpatternsによるパラメータ引き渡し

--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    apName = 'On RecordLapTime AP '
-    if 'msg' in request.GET:
-        msg = request.GET['msg']
-        result = apName + 'You typed: "' + msg + '".'
-    else:
-        result = apName + 'Please send msg parameter!'
+def index(request, id, nickname):
+    result = 'Your ID; ' + str(id) + ', name: "' \
+        + nickname + '".'
     return HttpResponse(result)
