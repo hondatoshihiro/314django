e9d76d1b3f899cc21d221a1f42eb21f9dd59ec04
hondatoshihiro <harutokaho@gmail.com>
2026-04-12 17:52:21 +0900

    複数ページの移動

--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -4,7 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     params = {
-        'title':'recordlaptime/index.html',
+        'title':'recordlaptime/index',
         'msg':'これはサンプルで作ったページです。',
+        'goto':'next',
+    }
+    return render(request, 'recordlaptime/index.html', params)
+
+def next(request):
+    params = {
+        'title':'recordlaptime/next',
+        'msg':'これはもう１つのサンプルページです。',
+        'goto':'index',
     }
     return render(request, 'recordlaptime/index.html', params)
