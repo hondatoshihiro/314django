f488c4b63876d8e25b0825bf31c6f434da7e2f7c
hondatoshihiro <harutokaho@gmail.com>
2026-04-12 10:21:21 +0900

    クエリパラメータの取得(request.GET)

--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello RecordLaptimeAP!!")
+    msg = request.GET['msg']
+    return HttpResponse('On RecordLaptimeAP You typed: "' + msg + '".')
