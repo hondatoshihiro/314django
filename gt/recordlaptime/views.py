f9cc7b2ca5a3cae875a2f2d6f634c57e634e1dde
hondatoshihiro <harutokaho@gmail.com>
2026-04-26 07:32:45 +0900

    formを使用したデータの送受信

--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import RecordLaptimeForm
 
 # Create your views here.
 def index(request):
     params = {
         'title':'recordlaptime/index',
-        'msg':'お名前は？',
+        'message':'your data:',
+        'form':RecordLaptimeForm()
     }
+    if (request.method == 'POST'):
+        params['message'] = '名前:' + request.POST['name'] + \
+            '<br>メール:' + request.POST['mail'] +\
+            '<br>年齢:' + request.POST['age']
+        params['form'] = RecordLaptimeForm(request.POST)
     return render(request, 'recordlaptime/index.html', params)
 
 def form(request):
