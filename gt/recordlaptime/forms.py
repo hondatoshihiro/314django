cfc98e12067a5cacf60b88e614acbb0eeeb7cb77
hondatoshihiro <harutokaho@gmail.com>
2026-04-26 11:31:47 +0900

    formの整形

--- gt/recordlaptime/forms.py
+++ gt/recordlaptime/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class RecordLaptimeForm(forms.Form):
-    name = forms.CharField(label='name')
-    mail = forms.CharField(label='mail')
-    age = forms.IntegerField(label='age')
+    #widget引数：forms.TextInputで<input type="text">のウィジェット
+    #forms.NumberINputで<input type="number">のウィジェットを埋め込む
+    name = forms.CharField(label='name', widget=forms.TextInput(attrs={'class':'form-control'}))
+    mail = forms.CharField(label='mail', widget=forms.TextInput(attrs={'class':'form-control'}))
+    age = forms.IntegerField(label='age', widget=forms.NumberInput(attrs={'class':'form-control'}))
