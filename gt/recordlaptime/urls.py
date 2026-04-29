2f3f984b2ffcbe9baee8c1fbee09d6e2c53359b4
hondatoshihiro <harutokaho@gmail.com>
2026-04-29 10:39:04 +0900

    viewクラスの使用

--- gt/recordlaptime/urls.py
+++ gt/recordlaptime/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import re_path
+from .views import RecordLaptimeView
 #この記載はなに(カレントディレクトリから、viewsを読み込むってこと)
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path(r'', RecordLaptimeView.as_view(), name='index'),
 ]
--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from .forms import RecordLaptimeForm
 
 
-class HelloView(TemplateView):
+class RecordLaptimeView(TemplateView):
 
     def __init__(self):
         self.params = {
@@ -14,7 +14,7 @@ class HelloView(TemplateView):
         }
 
     def get(self, request):
-        return render(request, 'recordlaptitme/index.html', self.params)
+        return render(request, 'recordlaptime/index.html', self.params)
     
     def post(self, request):
         msg = 'あなたは、<br>' + request.POST['name'] + \
@@ -24,25 +24,3 @@ class HelloView(TemplateView):
         self.params['message'] = msg
         self.params['form'] = RecordLaptimeForm(request.POST)
         return render(request, 'recordlaptime/index.html', self.params)
-
-# Create your views here.
-def index(request):
-    params = {
-        'title':'recordlaptime/index',
-        'message':'your data:',
-        'form':RecordLaptimeForm()
-    }
-    if (request.method == 'POST'):
-        params['message'] = '名前:' + request.POST['name'] + \
-            '<br>メール:' + request.POST['mail'] +\
-            '<br>年齢:' + request.POST['age']
-        params['form'] = RecordLaptimeForm(request.POST)
-    return render(request, 'recordlaptime/index.html', params)
-
-def form(request):
-    msg = request.POST['msg']
-    params = {
-        'title':'recordlaptime/form',
-        'msg':'こんにちは、' + msg + 'さん。',
-    }
-    return render(request, 'recordlaptime/index.html',params)
