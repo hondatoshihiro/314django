7ddb467ad06e850447a76a50403c0113e1512837
hondatoshihiro <harutokaho@gmail.com>
2026-04-29 18:33:02 +0900

    指定IDのデータ取得・表示

--- gt/recordlaptime/forms.py
+++ gt/recordlaptime/forms.py
@@ -1,8 +1,4 @@
 from django import forms
 
 class RecordLaptimeForm(forms.Form):
-    #widget引数：forms.TextInputで<input type="text">のウィジェット
-    #forms.NumberINputで<input type="number">のウィジェットを埋め込む
-    name = forms.CharField(label='name', widget=forms.TextInput(attrs={'class':'form-control'}))
-    mail = forms.CharField(label='mail', widget=forms.TextInput(attrs={'class':'form-control'}))
-    age = forms.IntegerField(label='age', widget=forms.NumberInput(attrs={'class':'form-control'}))
+    id = forms.IntegerField(label='ID')
--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from .forms import RecordLaptimeForm
 
 def index(request):
-    data = Friend.objects.all()
     params = {
         'title':'RecordLaptime',
         'message': 'all friends.',
-        'data': data,
+        'form': RecordLaptimeForm(),
+        'data': [],
     }
+    if (request.method == "POST"):
+        num = request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = RecordLaptimeForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
     return render(request, 'recordlaptime/index.html', params)
