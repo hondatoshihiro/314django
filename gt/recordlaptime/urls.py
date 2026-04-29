de9e1df62fa4ace19e8918ae32c57b9351dea5c4
hondatoshihiro <harutokaho@gmail.com>
2026-04-29 15:26:51 +0900

    DBのレコード表示

--- gt/recordlaptime/urls.py
+++ gt/recordlaptime/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
-from .views import RecordLaptimeView
+#from django.urls import re_path
+from django.urls import path
+from . import views
 #この記載はなに(カレントディレクトリから、viewsを読み込むってこと)
 
 urlpatterns = [
-    re_path(r'', RecordLaptimeView.as_view(), name='index'),
+#    re_path(r'', RecordLaptimeView.as_view(), name='index'),
+    path('', views.index, name='index'),
 ]
--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import RecordLaptimeForm
+from .models import Friend
 
-
-class RecordLaptimeView(TemplateView):
-
-    def __init__(self):
-        self.params = {
-            'title':'RecordLaptime',
-            'message':'Your data:',
-            'form': RecordLaptimeForm()
-        }
-
-    def get(self, request):
-        return render(request, 'recordlaptime/index.html', self.params)
-    
-    def post(self, request):
-        msg = 'あなたは、<br>' + request.POST['name'] + \
-            ' (' + request.POST['age'] + \
-            ') </b>さんです。<br>メールアドレスは<b>' + request.POST['mail'] + \
-            '</b>ですね。'
-        self.params['message'] = msg
-        self.params['form'] = RecordLaptimeForm(request.POST)
-        return render(request, 'recordlaptime/index.html', self.params)
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title':'RecordLaptime',
+        'message': 'all friends.',
+        'data': data,
+    }
+    return render(request, 'recordlaptime/index.html', params)
