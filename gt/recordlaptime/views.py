f448cd80b88d910d9e0a0f63e0af8ec55c5ec824
hondatoshihiro <harutokaho@gmail.com>
2026-05-01 14:38:55 +0900

    QuerySetの確認

--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
-from .forms import RecordLaptimeForm
 
 def index(request):
+    data = Friend.objects.all()
     params = {
         'title':'RecordLaptime',
-        'message': 'all friends.',
-        'form': RecordLaptimeForm(),
-        'data': [],
+        'data': data,
     }
-    if (request.method == "POST"):
-        num = request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = RecordLaptimeForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
     return render(request, 'recordlaptime/index.html', params)
