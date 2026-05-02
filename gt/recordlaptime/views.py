78b872965edff1b73b32e5654f32001ac5074ecc
hondatoshihiro <harutokaho@gmail.com>
2026-05-02 20:02:08 +0900

    QuerySetの表示

--- gt/recordlaptime/views.py
+++ gt/recordlaptime/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Friend
+from django.db.models import QuerySet
+
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+
+QuerySet.__str__ = __new_str__
 
 def index(request):
-    num = Friend.objects.all().count()
-    first = Friend.objects.all().first()
-    second = Friend.objects.all().last()
-    data = [num, first, second]
+    data = Friend.objects.all().values('id', 'name', 'age')
     params = {
         'title':'RecordLaptime',
         'data': data,
